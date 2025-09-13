3e258152217ef06c4301b1fb3c7134fea5614ff7
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2025-09-13 21:42:16 +0530

    board height updated

--- index.tsx
+++ index.tsx
@@ -2,9 +2,8 @@
 import { ArduinoShield } from "./ArduinoShield";
 
 export default () => (
-  <board width="50mm" height="50mm">
+  <board width="50mm" height="60mm">
     <ArduinoShield name="MyArduinoShield">
-      {/* Example: Add some components to the shield */}
       <resistor resistance="1k" footprint="0402" name="R1" pcbX={5} pcbY={5} />
       <capacitor capacitance="1000pF" footprint="0402" name="C1" pcbX={10} pcbY={5} />
       <trace from=".R1 > .pin1" to=".C1 > .pin1" />
