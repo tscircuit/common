ebcf79990f6c30a16a7b1253aba5687add2849aa
MayorChristopher <mayoru24@gmail.com>
2025-10-26 16:47:35 +0100

    Revert "Fix formatting with biome as requested by reviewer"
    This reverts commit 93c744a0684a460d5d9a2a89494a446f5fd3e141.

--- lib/ViaGridBoard/ViaGridBoardLarge.test.circuit.tsx
+++ lib/ViaGridBoard/ViaGridBoardLarge.test.circuit.tsx
@@ -1,3 +1,5 @@
 import { ViaGridBoard } from "./ViaGridBoard.circuit"
 
-export default () => <ViaGridBoard name="VG_LARGE" size="large" />
+export default () => (
+  <ViaGridBoard name="VG_LARGE" size="large" />
+)
--- lib/ViaGridBoard/ViaGridBoardSmall.test.circuit.tsx
+++ lib/ViaGridBoard/ViaGridBoardSmall.test.circuit.tsx
@@ -1,3 +1,5 @@
 import { ViaGridBoard } from "./ViaGridBoard.circuit"
 
-export default () => <ViaGridBoard name="VG_SMALL" size="small" />
+export default () => (
+  <ViaGridBoard name="VG_SMALL" size="small" />
+)
