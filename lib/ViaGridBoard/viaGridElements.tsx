6021468a0746545ef0f4c8d20eb9367fd69ffd8d
Ray <raykholo@users.noreply.github.com>
2025-10-25 21:08:56 -0400

    format!

--- lib/ViaGridBoard/viaGridElements.tsx
+++ lib/ViaGridBoard/viaGridElements.tsx
@@ -1,30 +1,29 @@
-import { CommonLayoutProps, GroupProps } from "@tscircuit/props";
+import { CommonLayoutProps, GroupProps } from "@tscircuit/props"
 
-
-export const ViaGridVia = (props:CommonLayoutProps) => {
-    return (
-        <via
-        fromLayer="top"
-        toLayer="bottom"
-        outerDiameter="1.5mm"
-        holeDiameter="0.3mm"
-        // pcbX={0}
-        // pcbY={0}
-        {...props}
-        />
-    );
+export const ViaGridVia = (props: CommonLayoutProps) => {
+  return (
+    <via
+      fromLayer="top"
+      toLayer="bottom"
+      outerDiameter="1.5mm"
+      holeDiameter="0.3mm"
+      // pcbX={0}
+      // pcbY={0}
+      {...props}
+    />
+  )
 }
 
-export const ViaGridPlus = (props:GroupProps) => {
-    return (
-        <group {...props}>
-            <ViaGridVia pcbX={0} pcbY={0}/>
-            <ViaGridVia pcbX={2.5} pcbY={0}/>
-            <ViaGridVia pcbX={0} pcbY={2.5}/>
-            <ViaGridVia pcbX={-2.5} pcbY={0}/>
-            <ViaGridVia pcbX={0} pcbY={-2.5}/>
-        </group>
-    )
+export const ViaGridPlus = (props: GroupProps) => {
+  return (
+    <group {...props}>
+      <ViaGridVia pcbX={0} pcbY={0} />
+      <ViaGridVia pcbX={2.5} pcbY={0} />
+      <ViaGridVia pcbX={0} pcbY={2.5} />
+      <ViaGridVia pcbX={-2.5} pcbY={0} />
+      <ViaGridVia pcbX={0} pcbY={-2.5} />
+    </group>
+  )
 }
 
 export default ViaGridPlus
