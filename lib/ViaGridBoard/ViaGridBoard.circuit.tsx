d2dcf44ac6da8dc3c21eb3b9bd23cd7936db749f
Ray <raykholo@users.noreply.github.com>
2025-10-25 21:05:00 -0400

    Initial completion of ViaGridBoard to best of ability w current tsc functionality
    Key points:
    
    -4 corner "pacman" are individually hardcoded while polygon rotation is implemented
    - Vias are all correctly placed but not named
    - copperpour is defined but does not appear to render? and outline/ clearance props are broken?
    - bottom text is silkscreen for now since I don't think copper text exists in tsc

---
+++ lib/ViaGridBoard/ViaGridBoard.circuit.tsx
@@ -0,0 +1,161 @@
+import { splitBoardAndChipProps } from "../../util/splitBoardAndChipProps"
+import { ChipProps, BoardProps } from "@tscircuit/props"
+import { grid } from "@tscircuit/math-utils"
+import { ViaGridVia, ViaGridPlus } from "./viaGridElements"
+
+import {pacmanPolygonPoints, PCMN1, PCMN2, PCMN3, PCMN4} from "./pacmanPolygon"
+
+
+type ViaGridBoardProps = ChipProps &
+  BoardProps & { children?: any; boardName?: string }
+
+//export const ViaGridBoard = (props: ViaGridBoardProps) => {
+export const ViaGridBoard = ({ children, ...rest }: ViaGridBoardProps) => {
+  const { boardProps, chipProps = {} } = splitBoardAndChipProps({
+    ...rest,
+  }) as {
+    boardProps: any
+    chipProps: Record<string, any>
+  }
+
+  const resolvedName = `${chipProps.name}_chip`
+  const { name: _, ...chipRest } = chipProps
+
+    return (
+        <board 
+        {...boardProps}
+        width="100mm"
+        height="65mm"
+        boardAnchorPosition={{x:0, y:0}} 
+        boardAnchorAlignment="bottom_left"
+        >
+
+          <pcbnoterect  //User.1 kicad rect shows outer bounds of usable area
+          pcbX={50}
+          pcbY={32.5}
+          width={90}
+          height={55}
+          strokeWidth={0.3}
+          color="blue"
+        />
+
+
+    {pacmanGridCells.map((cell) => (
+    <chip name={"PCMN"+cell.index}  pcbX={cell.center.x} pcbY={cell.center.y}
+    //pcbRotation="180deg"    //{(cell.index-1)*90}
+    footprint={
+      
+      <footprint>
+        <smtpad
+          pcbX="0mm"
+          pcbY="0mm"
+          layer="top"
+          shape="polygon"
+          
+          //radius="2.5mm"
+          
+          //width="5mm"
+          //height="5mm"
+          portHints={["pin1"]}
+
+          points={
+          (
+            cell.index ===0 ? PCMN1 :
+            cell.index ===1 ? PCMN4 :
+            cell.index ===2 ? PCMN2 :
+            cell.index ===3 ? PCMN3 :
+            []
+          )
+
+          }
+        />
+      </footprint>
+    } />
+    ))}
+
+    <ViaGridPlus pcbX={30} pcbY={25}/>
+    <ViaGridPlus pcbX={70} pcbY={25}/>
+    <ViaGridPlus pcbX={30} pcbY={40}/>
+    <ViaGridPlus pcbX={70} pcbY={40}/>
+
+    {horizontalEdgeViaGridCells.map((cell) => (
+      <ViaGridVia pcbX={cell.center.x} pcbY={cell.center.y} key={cell.index} />
+    ))}
+
+    {verticalEdgeViaGridCells.map((cell) => (
+      <ViaGridVia pcbX={cell.center.x} pcbY={cell.center.y} key={cell.index} />
+    ))}
+
+        <chip name="TOP_RECT" footprint={
+      <footprint>
+        <smtpad
+          pcbX="50mm"
+          pcbY="62.5mm"
+          layer="top"
+          shape="rect"
+          width="40mm"
+          height="3mm"
+          portHints={["pin1"]}
+          cornerRadius={0.5}
+        />
+      </footprint>
+    } />
+
+    <silkscreentext text="VIAGRID TOP" fontSize="1.5mm" pcbX={50} pcbY={2.5}/>
+
+    <copperpour connectsTo="net.GND" layer="top" 
+      // outline=[
+      //   { x: 5, y: 5 },
+      //   { x: 5, y: 60 },
+      //   { x: 95, y: 60 },
+      //   { x: 95, y: 5 }
+      // ]
+    />
+
+
+
+        
+        </board>
+    )
+}
+
+
+const pacmanGridCells = grid({
+rows: 2,
+cols: 2,
+// width: 90,
+// height: 55,
+xSpacing: 90, // if you want to provide the spacing instead of width
+ySpacing:55, // if you want to provide the spacing instead of height
+offsetX: 90/2 +5, // optional
+offsetY: 55/2 +5, // optional
+yDirection: "up-is-negative", // optional, default: "cartesian"
+// centered: true  // optional, default: true
+})
+
+
+const horizontalEdgeViaGridCells = grid({
+rows: 2,
+cols: 17,
+// width: 80,
+// height: 45,
+xSpacing: 5, // if you want to provide the spacing instead of width
+ySpacing:45, // if you want to provide the spacing instead of height
+offsetX: 90/2 +5, // optional
+offsetY: 55/2+5, // optional
+yDirection: "up-is-negative", // optional, default: "cartesian"
+// centered: true  // optional, default: true
+})
+
+const verticalEdgeViaGridCells = grid({
+rows: 8,
+cols: 2,
+// width: 80,
+// height: 45,
+xSpacing: 80, // if you want to provide the spacing instead of width
+ySpacing: 5, // if you want to provide the spacing instead of height
+offsetX: 90/2 +5, // optional
+offsetY: 55/2+5, // optional
+yDirection: "up-is-negative", // optional, default: "cartesian"
+// centered: true  // optional, default: true
+})
